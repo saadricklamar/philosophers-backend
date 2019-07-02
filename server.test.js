8508114f70e4ce067474eea2625eec9a73455e7e
Saad Baradan <saadbaradan@gmail.com>
2019-07-02 13:26:46 -0600

    Add Travis CI file

---
+++ server.test.js
@@ -0,0 +1,14 @@
+// import server from './server';
+// const environment = process.env.NODE_ENV || 'test'
+// const configuration = require('./knexfile')[environment]
+// const database = require('knex')(configuration)
+
+describe('server', () => {
+    describe('Get', () => {
+        it.skip('should say a equals b', () => {
+            const a = true;
+            const b = true;
+            expect(a).toEqual(b)
+        })
+    })
+})
